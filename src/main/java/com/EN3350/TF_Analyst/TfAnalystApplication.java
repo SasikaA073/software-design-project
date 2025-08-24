77f99716498ae5556017546a44d911d1eddeba94
SayuruA <sayurushenal141@gmail.com>
2025-08-24 14:58:30 +0530

    Init from Spring Initializr

---
+++ src/main/java/com/EN3350/TF_Analyst/TfAnalystApplication.java
@@ -0,0 +1,13 @@
+package com.EN3350.TF_Analyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfAnalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfAnalystApplication.class, args);
+	}
+
+}
