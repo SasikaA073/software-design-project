1f1bc8dadf5fe70bb1d7e7a1a5c0207cc421e38a
SayuruA <sayurushenal141@gmail.com>
2025-09-13 19:21:54 +0530

    Test relation between Transformer and Inspection, successful.

--- src/main/java/com/EN3350/TF_Analyst/TfAnalystApplication.java
+++ src/main/java/com/EN3350/TF_Analyst/TfAnalystApplication.java
@@ -1,13 +1,39 @@
 package com.EN3350.TF_Analyst;
 
+import com.EN3350.TF_Analyst.Inspection.Inspection;
+import com.EN3350.TF_Analyst.Inspection.InspectionRepository;
 import com.EN3350.TF_Analyst.Transformer.Transformer;
+import com.EN3350.TF_Analyst.Transformer.TransformerRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+
+import java.util.List;
 
 @SpringBootApplication
 public class TfAnalystApplication{
 	public static void main(String[] args) {
         SpringApplication.run(TfAnalystApplication.class, args);
 	}
+    @Bean
+    @Order(3)
+    @Transactional
+    public CommandLineRunner commandLineRunner(InspectionRepository inspectionRepository,
+                                               TransformerRepository transformerRepository) {
+        return args -> {
+//            Transformer transformer1 = transformerRepository.findById(1L).orElseThrow();
+            transformerRepository.findByIdWithInspections(1L)
+                    .ifPresent(transformer -> {
+                        System.out.println("fetch inspection lazy...");
+                        List<Inspection> inspections = transformer.getInspections();
+                        inspections.forEach(inspection -> {
+                            System.out.println(
+                                    transformer.getTransformerNo() + " has inspection " + inspection.getInspectionNo());
+                        });
+                    });
+        };
+    }
 }
--- src/main/java/com/EN3350/TF_Analyst/Transformer/TransformerRepository.java
+++ src/main/java/com/EN3350/TF_Analyst/Transformer/TransformerRepository.java
@@ -1,6 +1,8 @@
 package com.EN3350.TF_Analyst.Transformer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface TransformerRepository extends JpaRepository<Transformer, Long> {
 //    Optional<Transformer> findById(long id);
+    @Query("SELECT t FROM Transformer t LEFT JOIN FETCH t.inspections WHERE t.id = :id")
+    Optional<Transformer> findByIdWithInspections(@Param("id") Long id);
 }
